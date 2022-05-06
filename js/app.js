4f9b8a47baff151c868d07435309903dd5dc80ab
Yousef Al Farwan <yousefsyria304@gmail.com>
2022-05-06 17:03:26 +0300

    task 5 updated >>>

--- js/app.js
+++ js/app.js
@@ -1,31 +1,75 @@
-var name = prompt('enter you name please ! ')
-var gender = prompt('enter your gender please male or female ! ')
+//part 1 
+
+let userName = prompt('enter your name please !! ')
+let gender = prompt('enter your gender as male or female !!')
+let age = prompt('enter your age ..')
 
 if (gender=='male' || gender=='female'){
-
-    var age = prompt('enter your age please !')
-
-    if (age <=0){
-        alert('your age is invalid ')
+        if (age <=0 ) {
+            alert('your age is invalid .. try agin..')
+        }
+        else {
+            let skip = prompt('Enter yes if you want to skip welcome message ..')
+            if(skip=='yes'){
+                
+                // alert('welcome message skipped .. :(   ')
+            }
+            else{
+                if(gender=='male'){
+                    alert('welcome MR ' + userName + ' to our website :)  ')
+                }
+                else{
+                    alert('welcome MS ' + userName + ' to our website :)  ')
+                }
+            }
+        }
     }
     else{
-    var confirmation = prompt('enter yes to skip welcoming message ')
-    if (confirmation=='yes'){
-        alert('welcoming message skipped')
-    }
-    else{
-         if(gender=='male'){
-             alert('welcome MR '+name+' to TODO website...')
-         }
-         else{
-            alert('welcome MS '+name+' to TODO website...')
-         }
-    }
-    }
-
-    }
-    else {
-        alert('welcome to TODO website ... ')
+        let age = prompt('your gender is invalid please enter your age agian .. ')
+        if (age <=0){
+            alert('your age is invalid try again please ..')
+        }
+        else {
+            let skip = prompt('enter yes if you want to skip welcome message..')
+            if (skip=='yes'){
+                
+                // alert('welcome message skipped :(  ')
+            }
+            else{
+                    alert('welcome '+ userName + ' to our website :)  ')
+            }
+        }
     }
 
- 
+    // part 2 
+
+//     let questionOne = prompt('Are you enterd userName ? yes/no')
+//     let questionTow = prompt('Are you enterd gender ? yes/no')
+//     let questionThree = prompt('Are you enterd age ? yes/no')
+
+//   function yesOrNo(){
+
+//     let array = []
+
+//        if (questionOne=='yes'){
+//         array.push(userName)
+//        }else{array.push("invalid input")}
+
+
+//        if (questionTow=='yes'){
+//         array.push(gender)
+//        }else{array.push("invalid input")}
+
+       
+//        if (questionThree=='yes'){
+//         array.push(age)
+//        }else{array.push("invalid input")}
+
+
+
+//         console.log(array)
+// }
+// yesOrNo()
+
+// end of part 2
+
